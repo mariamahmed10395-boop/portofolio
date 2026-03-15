8bcaa3697d6b720599d25e13f45d90d573461215
mariam ahmed <mariamahmed10395@gmail.com>
2026-03-15 14:28:10 +0200

    portofolio

---
+++ script.js
@@ -0,0 +1,17 @@
+// smooth scrolling
+
+document.querySelectorAll('nav a').forEach(link => {
+
+link.addEventListener('click', function(e){
+
+e.preventDefault()
+
+const section = document.querySelector(this.getAttribute('href'))
+
+section.scrollIntoView({
+behavior:"smooth"
+})
+
+})
+
+})
